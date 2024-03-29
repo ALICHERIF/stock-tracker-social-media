ce06a0c889f87d5ade03e51db7a1f00b9e20b399
ALICHERIF <adem.mohamed@hotmail.fr>
2024-03-29 15:45:57 +0100

    add messages and nav bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "script"; // then your other imports that use `$`
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import * as jquery from "jquery"
+$(document).ready(function() {
+    $('.close').click(function() {
+        $(this).closest('.alert').fadeOut();
+    });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.3
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js" # @2.11.8
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
