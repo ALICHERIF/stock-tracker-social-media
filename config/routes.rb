5be5b1678f85044ac182033d05c88dffbfcd68fc
ALICHERIF <adem.mohamed@hotmail.fr>
2024-03-31 17:19:32 +0200

    setup front end structure for stock lookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
 end
