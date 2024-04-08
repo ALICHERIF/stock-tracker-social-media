632eb2089676fb986f0caebef7e202014aa2f25a
ALICHERIF <adem.mohamed@hotmail.fr>
2024-04-08 15:15:51 +0200

    last commit because rails 7 handle ajax request and turbolink replace it by turbo

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,27 @@
 class StocksController < ApplicationController
+ before_action :set_stock, only: [:search]
 
 def search
-  if params[:stock].present?
-  @stock = Stock.new_lookup(params[:stock])
-  if @stock
-    render 'users/my_portfolio'
-  else
-    flash[:alert] = "Please enter a valid symbol"
-    redirect_to my_portfolio_path
-  end
-else
-  flash[:alert] = "Please enter a symbol to search"
-  redirect_to my_portfolio_path
-end
-end
+   if @stock
+     respond_to do |format|
+       format.turbo do
+         render partial: 'users/result'
+       end
+     end
+   else
+     respond_to do |format|
+       format.turbo do
+         render partial: 'invalid_symbol'
+       end
+     end
+   end
+ end
+
+private
+
+ def set_stock
+   return unless params[:stock].present?
+   @stock = Stock.new_lookup(params[:stock])
+ end
 
 end
